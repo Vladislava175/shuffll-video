48b4c3fb846e2df9eb2ff461a2f4a50588d5f763
vladislava zhyguliov <vladislava.zhyguliovtsev@allcloud.io>
2023-12-19 15:05:16 +0200

    init utils

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { MaterialModule } from "./material/material.module";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { UtilsModule } from "./utils/utils/utils.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, DragDropModule],
+  exports: [DragDropModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
