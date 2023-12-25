23c3c3755a46fff5d07dfb84bb0ee8668b2ee143
vladislava zhyguliov <vladislava.zhyguliovtsev@allcloud.io>
2023-12-25 18:48:13 +0200

    seconds length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,20 +17,39 @@ export class AppComponent {
     'https://content.shuffll.com/files/background-music/1.mp4',
     'https://content.shuffll.com/files/background-music/1.mp4',
   ];
+  seconds: number[] = [];
   done: string[] = [];
   currentPlayingVideo!: HTMLVideoElement;
   videoIndex: number = 0;
+  dateSeconds!: Date;
 
   startNextVideo(isNext: boolean = false) {
+    !isNext && this.moveCoursor();
+    setInterval(() => {
+      this.moveCoursor();
+    }, 1000);
     if (this.videoIndex <= this.done.length) {
       isNext && this.videoIndex++;
       let video = document.getElementById(this.videoIndex + '') as any;
       video && video.play();
-    } else {
+    }
+    if (this.videoIndex == this.done.length) {
       this.videoIndex = 0;
     }
   }
-  drop(event: CdkDragDrop<string[]>) {
+  moveCoursor() {
+    let container = document.getElementById('ruler') as any;
+    let childToScrollTo = document.getElementById('vertical') as any;
+  }
+  fromListToList(event: CdkDragDrop<string[]>, toTrack: boolean = true) {
+    this.drop(event);
+    let video = event.item.element.nativeElement.childNodes[0] as any;
+    let length = toTrack
+      ? video.duration + this.seconds.length
+      : this.seconds.length - Math.round(video.duration);
+    this.seconds = Array.from({ length: length }, (_, i) => i + 1);
+  }
+  private drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
